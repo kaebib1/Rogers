11b026256bd92809bbb2fed8f2b004e530b3c117
kaebib1 <145460417+kaebib1@users.noreply.github.com>
2023-09-19 05:38:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_2frqr2i", "template_rflb8ms", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
